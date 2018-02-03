bfc334de544aceb5fd9bfb8b877ca99f0556107b
= <=>
2018-02-03 05:50:04 -0600

    peticiones http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProveedoresService } from './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+import { AddpresComponent } from './presupuestos/addpres/addpres.component';
+import { PresupuestosService } from './servicios/presupuestos.service';
+import { PresupuestosComponent } from './presupuestos/presupuestos/presupuestos.component';
+import { EditpresComponent } from './presupuestos/editpres/editpres.component';
+import { RegistroComponent } from './autenticacion/registro/registro.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: 'addprovee', component: AddproveeComponent },
+  { path: 'addpres', component: AddpresComponent},
+  { path: 'presupuestos', component: PresupuestosComponent },
+  { path: 'editpres/:id', component: EditpresComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: '**', component: InicioComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProveedoresComponent
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent,
+    AddproveeComponent,
+    AddpresComponent,
+    PresupuestosComponent,
+    EditpresComponent,
+    RegistroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [ProveedoresService],
+  providers: [ProveedoresService, PresupuestosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/presupuestos/presupuestos/presupuestos.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { PresupuestosService } from '../../servicios/presupuestos.service';
+
+@Component({
+  selector: 'app-presupuestos',
+  templateUrl: './presupuestos.component.html',
+  styleUrls: ['./presupuestos.component.css']
+})
+export class PresupuestosComponent implements OnInit {
+
+  presupuestos: any[] = [];
+
+
+  constructor(private presupuestosService: PresupuestosService) {
+    this.presupuestosService.getPresupuestos()
+        .subscribe(presupuestos => {
+           for ( const id$ in presupuestos) {
+             const p = presupuestos[id$];
+             p.id$ = id$;
+             this.presupuestos.push(presupuestos[id$]);
+          }
+        })
+  }
+
+  ngOnInit() {
+
+  }
+
+  eliminarPresupuesto(id$) {
+
+    this.presupuestosService.delPresupuesto(id$)
+      .subscribe( res => {
+        this.presupuestos = [];
+        this.presupuestosService.getPresupuestos()
+        .subscribe(presupuestos => {
+           for ( const id$ in presupuestos) {
+             const p = presupuestos[id$];
+             p.id$ = id$;
+             this.presupuestos.push(presupuestos[id$]);
+          }
+        })
+      });
+  }
+}
